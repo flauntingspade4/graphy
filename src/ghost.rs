3085ce3c1edc285567b03b1bf4d5ba13834e2769
flauntingspade4 <elliotwhybrow@gmail.com>
2021-05-28 19:50:40 +0100

    Update ghost.rs to allow some clippy lints, and rename methods

--- src/ghost.rs
+++ src/ghost.rs
@@ -1,3 +1,5 @@
+#![allow(clippy::module_name_repetitions)]
+
 use std::{cell::UnsafeCell, marker::PhantomData};
 
 #[derive(Clone, Copy, Default, Debug, Hash)]
@@ -15,6 +17,7 @@ pub struct GhostToken<'id> {
 }
 
 impl<'id> GhostToken<'id> {
+    #[allow(clippy::new_ret_no_self)]
     pub fn new<R>(f: impl for<'new_id> FnOnce(GhostToken<'new_id>) -> R) -> R {
         let token = Self::default();
         f(token)
@@ -41,10 +44,10 @@ impl<'id, T> GhostCell<'id, T> {
     pub fn get_mut(&mut self) -> &mut T {
         self.value.get_mut()
     }
-    pub const fn ghost_borrow<'a>(&'a self, _token: &'a GhostToken<'id>) -> &T {
+    pub const fn g_borrow<'a>(&'a self, _token: &'a GhostToken<'id>) -> &T {
         unsafe { &*self.value.get() }
     }
-    pub const fn ghost_borrow_mut<'a>(&'a self, _token: &'a mut GhostToken<'id>) -> &mut T {
+    pub const fn g_borrow_mut<'a>(&'a self, _token: &'a mut GhostToken<'id>) -> &mut T {
         unsafe { &mut *self.value.get() }
     }
 }
